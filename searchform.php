1b2e308650ff9cc520a3fd79e5b24a5f2b959c39
gpereiradasneves <gabriel.pereira@eccles.utah.edu>
2018-01-22 09:46:34 -0700

    Added google search engine

---
+++ searchform.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * The search-form template.
+ *
+ * @package Avada
+ * @subpackage Templates
+ */
+
+// Do not allow directly accessing this file.
+if ( ! defined( 'ABSPATH' ) ) {
+	exit( 'Direct script access denied.' );
+}
+/*
+?>
+<form role="search" class="searchform" method="get" action="<?php echo esc_url_raw( home_url( '/' ) ); ?>">
+	<div class="search-table">
+		<div class="search-field">
+			<input type="text" value="" name="s" class="s" placeholder="<?php esc_html_e( 'Search ...', 'Avada' ); ?>" required aria-required="true" aria-label="<?php esc_html_e( 'Search ...', 'Avada' ); ?>"/>
+		</div>
+		<div class="search-button">
+			<input type="submit" class="searchsubmit" value="&#xf002;" />
+		</div>
+	</div>
+</form>
+ 
+<?php
+  
+ 
+ */?>
+ <script>
+ (function() {
+   var cx = '010584092752809720990:80-yh2i4hwi';
+   var gcse = document.createElement('script');
+   gcse.type = 'text/javascript';
+   gcse.async = true;
+   gcse.src = 'https://cse.google.com/cse.js?cx=' + cx;
+   var s = document.getElementsByTagName('script')[0];
+   s.parentNode.insertBefore(gcse, s);
+ })();
+</script>
+<gcse:search></gcse:search>
