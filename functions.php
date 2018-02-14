4ad4f7150792204db71eb0938dbe46d299891506
gpereiradasneves <gabriel.pereira@eccles.utah.edu>
2018-02-14 11:18:05 -0700

    Created function to decide to display custom header

--- functions.php
+++ functions.php
@@ -216,7 +216,7 @@ function custom_header(){
 		$list_of_urls[0] = 'http://eccles.utah.edu';
 		$list_of_urls[1] = "//dmz-test.eccles.utah.edu/ecclesv5";
 		$list_of_urls[2] = '//localhost/Workspace/Wordpress_New/eccles/';
-		$list_of_urls[3] = 'http://dmz-test.eccles.utah.edu/undergraduate/';
+		
 		
 		$custom_header=TRUE;
 		$site_url = get_option( 'siteurl' );
