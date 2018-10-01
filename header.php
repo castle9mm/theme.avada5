3beede1cd67fcee672ebb36f256c771b2ebea860
gpereiradasneves <gabriel.pereira@eccles.utah.edu>
2018-10-01 14:15:02 -0600

    added tag manager code

--- header.php
+++ header.php
@@ -28,6 +28,19 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 })(window,document,'script','dataLayer','GTM-NZQ4NSL');</script>
 <!-- End Google Tag Manager -->
 
+
+
+<!-- Google Tag Manager Selva Buberci -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-TTMM274');</script>
+<!-- End Google Tag Manager -->
+
+
+
+
 		<?php Avada() -> head -> the_viewport(); ?>
 
 		<?php wp_head(); ?>
@@ -71,6 +84,13 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NZQ4NSL"
 height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
 <!-- End Google Tag Manager (noscript) -->
+
+
+<!-- Google Tag Manager (noscript) Selva Buberci-->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TTMM274"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
+
 
 		<?php do_action('avada_before_body_content');
 
