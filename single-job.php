f6a71bca9e94b205660a06eaf823655fcb85f478
gpereiradasneves <gabriel.pereira@eccles.utah.edu>
2018-10-11 14:12:30 -0600

    change single job post stile

--- single-job.php
+++ single-job.php
@@ -15,12 +15,15 @@ div, a, p, span,h1,h2,h3,h4{
 		font-size: 40px;
 		color: #cc0000;
 	}
+	.content div{
+		margin-bottom:10px;
+	}
 	
 </style>
 <div style="padding:10px">
 	<h1><?php echo $post->post_title; ?></h1>
 	
-	<div>
+	<div class="content">
 		<?php echo $post->post_content; ?>
 		</div>
 		
