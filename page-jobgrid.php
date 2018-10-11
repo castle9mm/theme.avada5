c43b6ea2a63ad1ec0804d1900d6824aaf62807ae
gpereiradasneves <gabriel.pereira@eccles.utah.edu>
2018-10-11 14:36:56 -0600

    change grid job style font

--- page-jobgrid.php
+++ page-jobgrid.php
@@ -4,6 +4,7 @@
 ?>
 <html>
 	<head>
+		 <link rel="stylesheet" type="text/css" href="<?php echo get_stylesheet_uri(); ?>">
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 		
 	
