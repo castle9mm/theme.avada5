24138ea17c507334748e3ba2f73390ac73a56caf
gpereiradasneves <gabriel.pereira@eccles.utah.edu>
2018-10-18 09:52:04 -0600

    Added cookie policy question on footer

--- footer.php
+++ footer.php
@@ -324,10 +324,10 @@ if(get_page_link()=="https://eccles.utah.edu/programs/undergraduate/schedule-an-
 </html>
 	  
 	  <script>
-	 /* var cookie=getCookie("policy-eccles");
+	 var cookie=getCookie("policy-eccles");
 	  //alert(cookie);
-	    if (cookie != "accepted") {
-	   		//document.write('<div id="policy-message"  style="padding-bottom: 10px;z-index: 9999999;position:fixed; bottom:0px;transition: all 2s linear;width:100%; background-color:#333;height: auto; text-align: center;"><div id="close-policy" onclick="setCookie();">x</div> <p style="color: #FFFFFF">We use cookies to ensure you get the best experience. By using our website you agree to our <a href="http://eccles.utah.edu/privacy-policy/">Cookie Policy</a>.</p> 	<input class="fusion-button button-flat fusion-button-round button-large button-default button-2" value="Got it!" type="button" onclick="setCookie();"/></div>');
+	    if (cookie != "accepte") {
+	   		document.write('<div id="policy-message"  style="padding-bottom: 10px;z-index: 9999999;position:fixed; bottom:0px;transition: all 2s linear;width:100%; background-color:#333;height: auto; text-align: center;"><div style="color: #FFFFFF; 		border-radius: 100%;		position: absolute;  		background-color: #cc0000;  		width:25px;		height: 25px;right:0; 		margin-right: 5px;	  		cursor: pointer;" onclick="setCookie();">x</div> <p style="color: #FFFFFF">We use cookies to ensure you get the best experience. By using our website you agree to our <a href="http://eccles.utah.edu/privacy-policy/">Cookie Policy</a>.</p> 	<input class="fusion-button button-flat fusion-button-round button-large button-default button-2" style="color:#ffffff" value="Got it!" type="button" onclick="setCookie();"/></div>');
 	    }
 	    
 	    
@@ -360,6 +360,6 @@ if(get_page_link()=="https://eccles.utah.edu/programs/undergraduate/schedule-an-
         }
     }
     return "";
-}*/
+}
 	 
 	  </script>
